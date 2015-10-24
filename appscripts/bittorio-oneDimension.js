94f6e5cd42c9f6832c62eac01d06294bba38ac7d
prashanthtr <prashanth.thattai@gmail.com>
2015-10-24 09:48:52 +0800

    added bittorio one-dimension. Time flow is from bottom to top. user can interact with a bunch of cells right at the bottom

--- appscripts/bittorio-oneDimension.js
+++ appscripts/bittorio-oneDimension.js
@@ -23,8 +23,8 @@ require(
         var pWidth = paper.canvas.offsetWidth;
         var pHeight = paper.canvas.offsetHeight;
         console.log("pWidth is " + pWidth + ", and pHeight is " + pHeight);
-        var xLen = 18, yLen = 9;
-        var xOffset = 1, yOffset = 3;
+        var xLen = 8, yLen = 6;
+        var xOffset = 1, yOffset = 1;
         //24,12
         // Just create a nice black background
         var bgRect = paper.rect(0,0,pWidth, pHeight);
@@ -52,87 +52,132 @@ require(
         var listNeighbours = [ [[13,14],[14,13]], [[15,13]], [[16,13],[17,14]], [[17,15]], [[17,16],[16,17]], [[15,17]], [[14,17],[13,16],], [[13,15]] ];
 
         var cnt = 0;
-        var timer = 0;
+        var timer = 1;
 
+        //bittorio display on which display happens
         var bittorio = [];
-        var i = 0, len = 49; //49 time units are displayed
-        for(; ++i < len;){
-            cnt = 0;
-
-            var bittorioRow = listBoundaryProperties.map (function (arr,ind){
-                var obj = paper.rect((cnt++ + xOffset)*xLen,((i+3)%53)*(yLen-2),7,7);
-                obj.type = "link";
-                obj.x = arr[0]%14;
-                obj.y = arr[1]%14;
-                obj.ind = ind;
-                //obj.attr({"fill": "red"});
-                obj.state = 1;
-                //updates the states of the cell based on adjacent values and environmental values
-
-                obj.updateState = function(prev, cur, next){
-                    var castate = prev + "" +  cur + ""+ next;
-                    //ca rule
-                    switch(castate){
-                    case "000": this.attr({"fill": "red"}); this.state = 1; break;
-                    case "001": this.attr({"fill": "white"}); this.state = 0; break;
-                    case "010": this.attr({"fill": "white"}); this.state = 0; break;
-                    case "011": this.attr({"fill": "red"}); this.state = 1;break;
-                    case "100": this.attr({"fill": "white"}); this.state = 0; break;
-                    case "101": this.attr({"fill": "white"}); this.state = 0; break;
-                    case "110": this.attr({"fill": "white"}); this.state = 0; break;
-                    case "111": this.attr({"fill": "white"}); this.state = 0; break;
-                    };
-                }
-                obj.updateState(1,obj.state,1);
-
-                obj.mousedown(function(){
-                    mouseDownState = 1;
-                });
-
-                obj.mouseup(function(){
-                    mouseDownState = 0;
-                });
-
-                //toggle state
-                obj.hover(function(){
-                    console.log("added click" + this.state);
-
-                    if( mouseDownState == 1 ){
-                        this.state = (this.state + 1)%2; //obj = (obj.state + 1)%2;
-                        if(this.state == 1){
-                            this.attr({"fill": "white"})
-                        }
-                        else{
-                            this.attr({"fill": "red"})
-                        }
+        var row = 0, col = 0, len = 60; //len-1 time units are displayed
+        var colLength = 50;
+        for(row = 0; row < len-1; row++){
+            bittorio[row] = [];
+            for(col=0; col< colLength; col++){
+                bittorio[row][col] = paper.rect((col+xOffset)*xLen,(row+yOffset)*yLen,7,7);
+                bittorio[row][col].attr({"fill": "white"});
+                bittorio[row][col].state = 0;
+                bittorio[row][col].update = function(){
+                    if( this.state == 0){
+                        this.attr({"fill": "white"});
                     }
-
-                });
-                obj.updateState();
-                return obj;
-            });
-            bittorio.push(bittorioRow);
+                    else{
+                        this.attr({"fill": "black"});
+                    }
+                }
+            }
         }
 
+        bittorio[len-1] = [];
+        for(col=0; col< colLength; col++){
+
+            bittorio[len-1][col] = paper.rect((col+xOffset)*xLen,(len+1+yOffset)*yLen,8,8);
+            bittorio[len-1][col].type = "link";
+            bittorio[len-1][col].state = 1;
+
+            //console.log(bittorio[len-1][col].state);
+            //updates the states of the cell based on adjacent values and environmental values
+            bittorio[len-1][col].updateState = function(prev, cur, next){
+                var castate = prev + "" +  cur + ""+ next;
+                console.log(castate);
+                //ca rule
+                switch(castate){
+                case "000": this.state = 1; this.attr({"fill": "black"}); break;
+                case "001": this.state = 0; this.attr({"fill": "white"}); break;
+                case "010": this.state = 0; this.attr({"fill": "white"}); break;
+                case "011": this.state = 1; this.attr({"fill": "black"});break;
+                case "100": this.state = 0; this.attr({"fill": "white"});break;
+                case "101": this.state = 0; this.attr({"fill": "white"}); break;
+                case "110": this.state = 0; this.attr({"fill": "white"}); break;
+                case "111": this.state = 0; this.attr({"fill": "white"}); break;
+                };
+            }
+            //bittorio[len-1][col].updateState(0,bittorio[len-1][col].state,0);
+
+            bittorio[len-1][col].mousedown(function(){
+                mouseDownState = 1;
+            });
+
+            bittorio[len-1][col].mouseup(function(){
+                mouseDownState = 0;
+            });
+
+            //toggle state
+            bittorio[len-1][col].hover(function(){
+                console.log("added click" + this.state);
+                this.state = (this.state + 1)%2; //bittorio[len-1][col] = (bittorio[len-1][col].state + 1)%2;
+                if(this.state == 1){
+                    this.attr({"fill": "white"})
+                }
+                else{
+                    this.attr({"fill": "black"})
+                }
+            });
+
+        }
+
+        //entering the function once, it updates the state of the
+        //bittorio and stores in the new bittorio row.
         function caUpdate(){
-            console.log("updating");
-            console.log(timer%48);
-            bittorio[timer%48] = bittorio[timer%48].map( function (el,ind,arr){
+
+            //creating the new array from the last array
+            var newArr = bittorio[len-1].map( function (el,ind,arr){
                 var prev = [];
                 if( ind - 1 < 0){
-                    prev = arr[arr.length-1].state;
+                    prev = arr[arr.length-1].state; //turn around
                 }
                 else {
-                    prev = arr[ Math.abs(ind-1)%arr.length].state;
+                    prev = arr[ Math.abs(ind-1)%arr.length].state; //turn around
                 }
                 var next = arr[ Math.abs(ind+1)%arr.length].state;
+                //console.log("ind is" + ind + "," + prev + ", " + next);
                 el.updateState( prev , el.state, next);
                 return el;
             });
+
+            //updating all the previous arrays
+            var row = 0;
+            while( row < bittorio.length-1 ){
+
+                for(col=0; col < bittorio[row].length; col++){
+                    bittorio[row][col].state = bittorio[row+1][col].state;
+                    bittorio[row][col].update();
+                }
+                row++;
+            }
+
+            //updating the last array
+            for(col=0; col < bittorio[row].length; col++){
+                bittorio[bittorio.length-1][col] = newArr[col];
+            }
+            console.log(timer);
         }
 
+        //current timer - or the now row
+        var run = null;
 
-        var run = setInterval(request , parseFloat(document.getElementById("loopTime").value)); // start setInterval as "run"
+        document.getElementById('start').addEventListener("click", function(){
+            if(run == null){
+                run = setInterval(request , parseFloat(document.getElementById("loopTime").value)); // start setInterval as "run";
+            }
+        },true);
+
+
+        document.getElementById('stop').addEventListener("click", function(){
+            if(run != null){
+                clearInterval(run); // stop the setInterval()
+                run = null;
+            }
+        },true);
+
+
         function request() {
             //console.log(); // firebug or chrome log
             clearInterval(run); // stop the setInterval()
@@ -141,96 +186,5 @@ require(
             run = setInterval(request, parseFloat(document.getElementById("loopTime").value)); // start the setInterval()
         }
 
-        // setInterval(function(){
-        //     caUpdate();
-        //     timer++;
-        // }, parseFloat(document.getElementById("loopTime").value));
-
-        //initially it is not clear what ar the holes.
-        function movement(){
-
-            //for each particle, including the catalyst,
-
-            //hole near hole, no motion
-            // hole near bonded link, no motion
-            // hole near substrate, or catalyst, swap
-
-            var i = 0,j=0, len = listHoles.length;
-
-            for(; ++i < len;){
-                for(; ++j < len;){
-                    var randSel = Math.floor( 3*Math.random + 0.4);
-                    switch(randSel){
-                    case 0:
-                        if( arrCmp(listHoles, [i,j-1]) == 1 ){
-                        }
-                        else if( arrCmp(listBonded, [i,j-1]) == -1 ){
-
-                        }
-                        else if (arrCmp(listProducts, [i,j-1]) == -1){
-
-                        }
-                        else{ //substrates
-                            var temp = cellularArray[i][j-1];
-                            cellularArray[i][j-1] = cellularArray[i][j];
-                            cellularArray[i][j] = temp;
-                        }
-                        break;
-
-                    case 1:
-                        if( arrCmp(listHoles, [i-1,j]) == 1 ){
-                        }
-                        else if( arrCmp(listBonded, [i-1,j]) == -1 ){
-
-                        }
-                        else if (arrCmp(listProducts, [i-1,j]) == -1){
-
-                        }
-                        else{ //substrates
-                            var temp = cellularArray[i-1][j];
-                            cellularArray[i-1][j] = cellularArray[i][j];
-                            cellularArray[i][j] = temp;
-                        }
-                        break;
-
-                    case 2:
-                        if( arrCmp(listHoles, [i,j+1]) == 1 ){
-                        }
-                        else if( arrCmp(listBonded, [i,j+1]) == -1 ){
-
-                        }
-                        else if (arrCmp(listProducts, [i,j+1]) == -1){
-
-                        }
-                        else{ //substrates
-                            var temp = cellularArray[i][j+1];
-                            cellularArray[i][j+1] = cellularArray[i][j];
-                            cellularArray[i][j] = temp;
-                        }
-                        break;
-
-                    case 3:
-                        if( arrCmp(listHoles, [i+1,j]) == 1 ){
-                        }
-                        else if( arrCmp(listBonded, [i+1,j]) == -1 ){
-
-                        }
-                        else if (arrCmp(listProducts, [i+1,j]) == -1){
-
-                        }
-                        else{ //substrates
-                            var temp = cellularArray[i+1][j];
-                            cellularArray[i+1][j] = cellularArray[i][j];
-                            cellularArray[i][j] = temp;
-                        }
-                        break;
-
-                    };
-
-                }
-
-            }
-
-        }
 
 });
