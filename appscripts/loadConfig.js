907fa12e206306c2393db57f9eea27f72d88d9fd
prashanthtr <prashanth.thattai@gmail.com>
2015-11-18 10:24:13 +0800

    fixed rule 133, preset configuration problem

--- appscripts/loadConfig.js
+++ appscripts/loadConfig.js
@@ -11,13 +11,14 @@ define(
                 document.getElementById('carule').value = 132;
                 var str2 = utils.convert2Binary (132, 8);
                 document.getElementById('carulebinary').value = str2;
+                document.getElementById('wrapCells').value = "YES";
 
             }
-            else if( str == "localChanges"){
+            else if( str == "doublePerturb"){
                 document.getElementById('carule').value = 133;
                 var str2 = utils.convert2Binary (133, 8);
                 document.getElementById('carulebinary').value = str2;
-
+                document.getElementById('wrapCells').value = "YES";
             }
         }
 
